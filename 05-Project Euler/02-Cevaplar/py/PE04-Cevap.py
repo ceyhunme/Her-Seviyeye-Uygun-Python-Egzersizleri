41d5083de6a813494bf7b4f1ff2a43a1591b5aaf
Ceyhun Özdemir <iletisim@ceyhunozdemir.com>
2020-05-27 15:55:26 +0300

    Delete PE04-Cevap.py

--- 05-Project Euler/02-Cevaplar/py/PE04-Cevap.py
+++
@@ -1,9 +0,0 @@
-palindromSayilar = list()
-for i in range(100,1000):
-    for j in range(100,1000):
-        carpim = i *j
-        if str(carpim)==str(carpim)[::-1]:
-            palindromSayilar.append(carpim)
-max(palindromSayilar)
-            
-            
